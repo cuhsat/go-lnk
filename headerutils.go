192523902dc4234759c913cf64b2072b708904f8
Brian McKinney <brian.mckinney@elastic.co>
2025-12-03 08:40:30 -0600

    add a safety check for the header filetimes conversion

--- headerutils.go
+++ headerutils.go
@@ -66,7 +66,7 @@ func HotKey(hotkey uint16) string {
 
 	switch {
 	case 0x30 <= lb && lb <= 0x5A:
-		sb.WriteString(string(lb))
+		sb.WriteString(string(rune(lb)))
 	case 0x70 <= lb && lb <= 0x87:
 		sb.WriteString("F" + strconv.Itoa(int(lb-0x70+1)))
 	case lb == 0x90:
@@ -83,10 +83,19 @@ func HotKey(hotkey uint16) string {
 
 // toTime converts an 8-byte Windows Filetime to time.Time.
 func toTime(t [8]byte) time.Time {
+	// unix epoch in 100ns intervals
+	const epoch int64 = 116444736000000000
+
 	// Taken from https://golang.org/src/syscall/types_windows.go#L352, which is only available on Windows
 	nsec := int64(binary.LittleEndian.Uint32(t[4:]))<<32 + int64(binary.LittleEndian.Uint32(t[:4]))
+
+	// if the time is before unix epoch, return a zero time
+	if nsec < epoch {
+		return time.Time{}
+	}
+
 	// change starting time to the Epoch (00:00:00 UTC, January 1, 1970)
-	nsec -= 116444736000000000
+	nsec -= epoch
 	// convert into nanoseconds
 	nsec *= 100
 	return time.Unix(0, nsec)
--- headerutils_test.go
+++ headerutils_test.go
@@ -2,6 +2,7 @@ package lnk
 
 import (
 	"testing"
+	"time"
 )
 
 func TestHotKey(t *testing.T) {
@@ -33,6 +34,66 @@ func TestHotKey(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := HotKey(tt.args.hotkey); got != tt.want {
 				t.Errorf("HotKey() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_toTime(t *testing.T) {
+	type args struct {
+		ft [8]byte // filetime as read from disk
+	}
+	tests := []struct {
+		name string
+		args args
+		want time.Time
+	}{
+		{
+			name: "zero-time",
+			args: args{ft: [8]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}},
+			want: time.Time{},
+		},
+		{
+			name: "unix-epoch",
+			args: args{ft: [8]byte{0x00, 0x80, 0x3E, 0xD5, 0xDE, 0xB1, 0x9D, 0x01}},
+			want: time.Unix(0, 0),
+		},
+		{
+			name: "year-2000",
+			args: args{ft: [8]byte{0x00, 0xB0, 0x07, 0x70, 0x1D, 0x54, 0xBF, 0x01}},
+			want: time.Date(2000, 1, 1, 0, 0, 0, 0, time.Local),
+		},
+		{
+			name: "year-2023",
+			args: args{ft: [8]byte{0x00, 0x70, 0x5D, 0x48, 0xA6, 0x1D, 0xD9, 0x01}},
+			want: time.Date(2023, 1, 1, 0, 0, 0, 0, time.Local),
+		},
+		{
+			name: "with-time-components",
+			args: args{ft: [8]byte{0x80, 0xF8, 0x46, 0x29, 0x0F, 0x1E, 0xD9, 0x01}},
+			want: time.Date(2023, 1, 1, 12, 30, 45, 0, time.Local),
+		},
+		{
+			name: "with-microseconds",
+			args: args{ft: [8]byte{0x40, 0xB2, 0x33, 0xA6, 0xF1, 0x4C, 0xA3, 0x01}},
+			want: time.Date(1975, 1, 1, 0, 0, 0, 100000000, time.Local),
+		},
+		{
+			name: "leap-year-feb29",
+			args: args{ft: [8]byte{0x00, 0xF0, 0x66, 0x36, 0x7A, 0x82, 0xBF, 0x01}},
+			want: time.Date(2000, 2, 29, 0, 0, 0, 0, time.Local),
+		},
+		{
+			name: "end-of-day",
+			args: args{ft: [8]byte{0xC0, 0x2D, 0x62, 0x9A, 0xE6, 0x54, 0xBF, 0x01}},
+			want: time.Date(2000, 1, 1, 23, 59, 59, 900000000, time.Local),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := toTime(tt.args.ft)
+			if got != tt.want {
+				t.Errorf("%s: toTime() = %v, want %v", tt.name, got, tt.want)
 			}
 		})
 	}
